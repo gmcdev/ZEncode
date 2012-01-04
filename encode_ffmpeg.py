63ba9a1d78d97e055f104f3e3164031ada9573f9
Greg Connell <greg.connell@ff0000.com>
2012-01-04 11:14:24 -0800

    version 1.5, centralized ffmpeg settings

--- encode_ffmpeg.py
+++ encode_ffmpeg.py
@@ -1,27 +1,17 @@
 import sys, getopt, re
 from subprocess import Popen, PIPE, STDOUT
 
-opts = getopt.getopt( sys.argv[1:], 'p:t:s:b:a', ['path', 'target', 'size', 'bitrate', 'audio'] )
-
+print 'encode_ffmpeg.py -f '
+opts = getopt.getopt( sys.argv[1:], 'f', ['ffmpegcmd'] )
 #print 'Opts:', opts
-path = opts[0][0][1]
-target = opts[0][1][1]
-size = opts[0][2][1]
-bitrate = opts[0][3][1]
-audio = ''
-if len( opts[0] ) < 5:
-	audio = '-an'
-#print path, target, bitrate, audio
+#cmd = '/usr/local/bin/ffmpeg -i %s -y %s -s %s -vcodec libx264 -r 24 -b %sk -qcomp 1 -b_strategy 2 -subq 7 %s' % ( path_to_target, audio, size, bitrate, path_to_result )
+try:
+	cmd = opts[1][0]
+except:
+	print '-f (--ffmpegcmd) must be specified with a valid ffmpeg command' 
+	sys.exit( 2 )
 
-path_to_target = '%s%s' % ( path, target )
-extension_re = re.compile( '\..+$' )
-result_file = extension_re.sub( '.mp4', target ) 
-path_to_result = '%s%s' % ( path, result_file )
-#print path_to_target, path_to_result
-
-cmd = '/usr/local/bin/ffmpeg -i %s -y %s -s %s -vcodec libx264 -r 24 -b %sk -qcomp 1 -b_strategy 2 -subq 7 %s' % ( path_to_target, audio, size, bitrate, path_to_result )
-print cmd
-ffmpeg_stdout = Popen( cmd, shell=True, stdout=PIPE, stderr=STDOUT )
+print cmd[1:-1] + '\n'
+ffmpeg_stdout = Popen( cmd[1:-1], shell=True, stdout=PIPE, stderr=STDOUT )
 ffmpeg_result = ffmpeg_stdout.communicate()[0]
 print ffmpeg_result
-print '<AS3><resultFile>' + result_file + '</resultFile></AS3>'
